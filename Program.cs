6920ba046b502032798de70e11865f3daddc9079
konraddarnok <35291936+konraddarnok@users.noreply.github.com>
2020-06-09 09:34:57 +0200

    TCP Server

--- Program.cs
+++ Program.cs
@@ -1,35 +1,86 @@
-﻿using System;
+﻿using Opg1StaticMethod;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Net;
 using System.Net.Sockets;
+using System.Threading.Tasks;
 
-namespace OPG3TCPCLIENT
+namespace OPG3TCPSERVER
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            //Opret server
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            //Opret adresse eller port
+            TcpListener serverSocket = new TcpListener(ip, 7000);
+            //starter Server listener
+            serverSocket.Start();
+            Console.WriteLine("Server Started");
 
-            Console.WriteLine("Hello World it is client!");
-            Console.ReadLine();
+            //While loekke der holder serveren koerende, og ventende paa clienter
+            while (true)
+            {
+                //threading der tillader flere clienter at forbinde
+                Task.Run(() =>
+                {               
+                    //Venter på client forbindelse
+                    TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+                    Console.WriteLine("Client Connected");
+                    //kalder metoden DoClient ved forbindelses oprettelse
+                    DoClient(connectionSocket);
 
+                });
 
-            TcpClient clientSocket = new TcpClient("localhost", 7000);
+            }
 
-
-            Stream ns = clientSocket.GetStream();  //provides a Stream
+        }
+        //TCP client til modsvar fra server til client
+        public static void DoClient(TcpClient socket)
+        {
+            Stream ns = socket.GetStream();
             StreamReader sr = new StreamReader(ns);
             StreamWriter sw = new StreamWriter(ns);
-            sw.AutoFlush = true; // enable automatic flushing
+            sw.AutoFlush = true;
+            string message = sr.ReadLine();
 
-            string message = Console.ReadLine();
-            sw.WriteLine(message);
-            string serverAnswer = sr.ReadLine();
-            Console.WriteLine("Server: " + serverAnswer);
+            while (message != null && message != "")
+            {
+                //Laver clientens besked om til et array
+                string[] messageArray = message.Split(' ');
+
+                //opdeler kommando og parametre
+                string param = message.Substring(message.IndexOf(' ') + 1);
+                string command = messageArray[0];
+
+                switch (command)
+                {
+                    case "Gennemsnit":
+                        string[] par = param.Split(' ');
+                        sw.WriteLine("Gennemsnits braendstofsforbruget:" + TransportCalc.Average(int.Parse(par[0]), double.Parse(par[1])));
+
+                        break;
+                    case "TotalKm":
+
+                        sw.WriteLine("Summen af km koert:" + TransportCalc.Total(param));
+
+                        break;
+
+                    default:
+
+                        sw.WriteLine("Ugyldig kommando");
+                        break;
+                }
+                message = sr.ReadLine();
+            }
+
 
             ns.Close();
+            socket.Close();
 
-            clientSocket.Close();
         }
     }
-
 }

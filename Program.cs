2be08f38b263f294024dfdf10692e509cf1f1886
konraddarnok <35291936+konraddarnok@users.noreply.github.com>
2020-06-09 09:34:29 +0200

    TCP Client

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace OPG3TCPCLIENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World it is client!");
+            Console.ReadLine();
+
+
+            TcpClient clientSocket = new TcpClient("localhost", 7000);
+
+
+            Stream ns = clientSocket.GetStream();  //provides a Stream
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true; // enable automatic flushing
+
+            string message = Console.ReadLine();
+            sw.WriteLine(message);
+            string serverAnswer = sr.ReadLine();
+            Console.WriteLine("Server: " + serverAnswer);
+
+            ns.Close();
+
+            clientSocket.Close();
+        }
+    }
+
+}
